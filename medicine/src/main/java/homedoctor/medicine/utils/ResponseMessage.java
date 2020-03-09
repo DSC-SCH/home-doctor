456ed9b307ad0a88226b6cc29b97f5624a9831c7
Nathan <skeks463@gmail.com>
2020-03-09 21:45:54 +0900

    Add ResponseMessage

---
+++ medicine/src/main/java/homedoctor/medicine/utils/ResponseMessage.java
@@ -0,0 +1,46 @@
+package homedoctor.medicine.utils;
+
+public class ResponseMessage {
+
+    // ALARM RESPONSE MESSAGE
+    public static final String ALARM_CREATE_SUCCESS        = "알람 등록 성공";
+    public static final String ALARM_CREATE_FAIL           = "알람 등록 실패";
+    public static final String ALARM_UPDATE_SUCCESS        = "알람 수정 성공";
+    public static final String ALARM_UPDATE_FAIL           = "알람 수정 실패";
+    public static final String ALARM_DELETE_SUCCESS        = "알람 삭제 성공";
+    public static final String ALARM_DELETE_FAIL           = "알람 삭제 실패";
+    public static final String ALARM_SEARCH_SUCCESS        = "알람 조회 성공";
+    public static final String ALARM_SEARCH_FAIL           =  "알람 실패 성공";
+    // LABEL
+    public static final String LABEL_CREATE_SUCCESS        = "라벨 등록 성공";
+    public static final String LABEL_CREATE_FAIL           = "라벨 등록 실패";
+    public static final String LABEL_UPDATE_SUCCESS        = "라벨 수정 성공";
+    public static final String LABEL_UPDATE_FAIL           = "라벨 수정 실패";
+    public static final String LABEL_DELETE_SUCCESS        = "라벨 삭제 성공";
+    public static final String LABEL_DELETE_FAIL           = "라벨 삭제 실패";
+    public static final String NOT_FOUND_LABEL             = "등록된 라벨이 없습니다.";
+
+    // CONNECT
+    public static final String CONNECTION_CREATE_SUCCESS   = "연동 성공";
+    public static final String CONNECTION_CREATE_FAIL      = "연동 실패";
+    public static final String CONNECTION_DELETE_SUCCESS   = "연동 해제 성공";
+    public static final String CONNECTION_DELETE_FAIL      = "연동 해제 실패";
+    public static final String NOT_FOUND_CONNECTION        = "연동된 유저가 없습니다.";
+
+    // CONNECT CODE
+    public static final String CODE_CREATE_SUCCESS         = "코드 생성 성공";
+    public static final String CODE_CREATE_FAIL            = "코드 생성 실패";
+
+    // Prescription Image
+    public static final String PRESCRIPTION_CREATE_SUCCESS = "처방전 등록 성공";
+    public static final String PRESCRIPTION_CREATE_FAIL    = "처방전 등록 실패";
+    public static final String PRESCRIPTION_UPDATE_SUCCESS = "처방전 수정 성공";
+    public static final String PRESCRIPTION_UPDATE_FAIL    = "처방전 수정 실패";
+    public static final String PRESCRIPTION_DELETE_SUCCESS = "처방전 삭제 성공";
+    public static final String PRESCRIPTION_DELETE_FAIL    = "처방전 삭제 실패";
+    public static final String NOT_FOUND_PRESCRIPTION      = "등록된 처방전이 없습니다.";
+
+    // MEDICINE
+    public static final String MEDICINE_SEARCH_SUCCESS     = "약품 조회 성공";
+    public static final String NOT_FOUND_MEDICINE          = "입력된 정보가 포함된 약품이 없습니다.";
+}
