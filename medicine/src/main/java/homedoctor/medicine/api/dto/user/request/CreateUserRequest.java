6643c83244c9a733420f367557ea127acf10a5f2
Nathan <skeks463@gmail.com>
2020-03-09 21:45:39 +0900

    Add StatusCode

---
+++ medicine/src/main/java/homedoctor/medicine/api/dto/user/request/CreateUserRequest.java
@@ -0,0 +1,32 @@
+package homedoctor.medicine.api.dto.user;
+
+
+import homedoctor.medicine.domain.GenderType;
+import homedoctor.medicine.domain.SnsType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+@Data
+public class CreateUserRequest {
+
+    private String username;
+
+    private String birthday;
+
+    private String email;
+
+    private String kakaoId;
+
+    private String googleId;
+
+    private SnsType snsType;
+
+    private GenderType genderType;
+
+    private String phoneNum;
+}
---
+++ medicine/src/main/java/homedoctor/medicine/utils/StatusCode.java
@@ -0,0 +1,21 @@
+package homedoctor.medicine.utils;
+
+/**
+ * REF - https://documentation.commvault.com/commvault/v11/article?p=45599.htm
+ */
+public class StatusCode {
+
+    public static final int OK = 200;
+    public static final int CREATED = 201;
+    public static final int BAD_REQUEST = 400;
+    public static final int UNAUTHORIZED = 401;
+    public static final int FORBIDDEN = 403;
+    public static final int NOT_FOUND = 404;
+
+    // The HTTP method in the request was not supported by the resource. For example, the DELETE method cannot be used with the Agent API.
+    public static final int METHOD_NOT_ALLOWED = 404;
+    public static final int CONFLICT = 409;
+    public static final int INTERNAL_SERVER_ERROR = 500;
+    public static final int SERVICE_UNAVAILABLE = 503;
+    public static final int DB_ERROR = 600;
+}
