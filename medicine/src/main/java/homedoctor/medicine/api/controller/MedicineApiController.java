33cc4efc94512043e58b83fcf38f8c996030c8e7
Nathan <skeks463@gmail.com>
2020-03-12 01:35:25 +0900

    Add MedicineApiController

---
+++ medicine/src/main/java/homedoctor/medicine/api/controller/MedicineApiController.java
@@ -0,0 +1,97 @@
+package homedoctor.medicine.api.controller;
+
+import homedoctor.medicine.api.dto.DefaultResponse;
+import homedoctor.medicine.api.dto.medicine.MedicineDto;
+import homedoctor.medicine.common.ResponseMessage;
+import homedoctor.medicine.common.StatusCode;
+import homedoctor.medicine.domain.Medicine;
+import homedoctor.medicine.service.MedicineService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RestController
+@AllArgsConstructor
+public class MedicineApiController {
+
+    private final MedicineService medicineService;
+
+    @GetMapping("/search/name/{name}")
+    public DefaultResponse searchMedicineByName(
+            @PathVariable("name") String name) {
+        try {
+            List<Medicine> medicineList = (List<Medicine>) medicineService.findByName(name).getData();
+
+            List<MedicineDto> medicineDtoList = medicineList.stream()
+                    .map(m -> MedicineDto.builder()
+                            .medicineId(m.getId())
+                            .name(m.getName())
+                            .effect(m.getEffect())
+                            .build())
+                    .collect(Collectors.toList());
+
+            return DefaultResponse.response(StatusCode.OK,
+                    ResponseMessage.MEDICINE_SEARCH_SUCCESS,
+                    medicineDtoList);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return DefaultResponse.response(StatusCode.INTERNAL_SERVER_ERROR,
+                    ResponseMessage.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/search/keyword/{keyword}")
+    public DefaultResponse searchMedicineByKeyword(
+            @PathVariable("keyword") String keyword) {
+        try {
+            List<Medicine> medicineList = (List<Medicine>) medicineService.findByKeyword(keyword).getData();
+
+            List<MedicineDto> medicineDtoList = medicineList.stream()
+                    .map(m -> MedicineDto.builder()
+                            .medicineId(m.getId())
+                            .name(m.getName())
+                            .effect(m.getEffect())
+                            .build())
+                    .collect(Collectors.toList());
+
+            return DefaultResponse.response(StatusCode.OK,
+                    ResponseMessage.MEDICINE_SEARCH_SUCCESS,
+                    medicineDtoList);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return DefaultResponse.response(StatusCode.INTERNAL_SERVER_ERROR,
+                    ResponseMessage.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/search/{medicine_id}")
+    public DefaultResponse getMedicineDetail(
+            @PathVariable("medicine_id") Long id) {
+        try {
+            Medicine medicine = (Medicine) medicineService.findOneMedicine(id).getData();
+
+            MedicineDto medicineDto = MedicineDto.builder()
+                    .name(medicine.getName())
+                    .effect(medicine.getEffect())
+                    .dosage(medicine.getDosage())
+                    .badEffect(medicine.getBadEffect())
+                    .saveMethod(medicine.getSaveMethod())
+                    .validDate(medicine.getValidDate())
+                    .build();
+
+            return DefaultResponse.response(StatusCode.OK,
+                    ResponseMessage.MEDICINE_SEARCH_SUCCESS,
+                    medicineDto);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return DefaultResponse.response(StatusCode.INTERNAL_SERVER_ERROR,
+                    ResponseMessage.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
