619dc9e02667663d2baf75a9872be9e27488173d
Nathan <skeks463@gmail.com>
2020-03-12 21:04:28 +0900

    Add fetch Join in JPA Query & Convert implicit join to explicit join

--- medicine/src/main/java/homedoctor/medicine/repository/PrescriptionImageRepository.java
+++ medicine/src/main/java/homedoctor/medicine/repository/PrescriptionImageRepository.java
@@ -20,19 +20,27 @@ public class PrescriptionImageRepository {
     }
 
     public PrescriptionImage findOne(Long id) {
-        return em.find(PrescriptionImage.class, id);
+        String query = "select p from PrescriptionImage p join fetch p.alarm where p.id = :id";
+        return em.createQuery(query, PrescriptionImage.class)
+                .setParameter("id", id)
+                .getSingleResult();
     }
 
     public List<PrescriptionImage> findAllByAlarm(Alarm alarm) {
-        return em.createQuery("select p from PrescriptionImage p where p.alarm = :alarm", PrescriptionImage.class)
+        String query = "select p from PrescriptionImage p join fetch p.alarm where p.alarm = :alarm";
+
+        return em.createQuery(query, PrescriptionImage.class)
                 .setParameter("alarm", alarm)
                 .getResultList();
     }
 
     // 명시적 Join 사용하기.
     public List<PrescriptionImage> findAllByUser(User user) {
+        String query = "select p from PrescriptionImage p join fetch " +
+                "p.alarm join p.alarm a where a.user.id = :id";
+
         List<PrescriptionImage> prescriptionImages =
-                em.createQuery("select p from PrescriptionImage p where p.alarm.user.id = :id",
+                em.createQuery(query,
                         PrescriptionImage.class)
                 .setParameter("id", user.getId())
                 .getResultList();
